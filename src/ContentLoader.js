7cd23537a267dee6444be195e695fe4f8bd50064
Salman Salari <salman@aleph-labs.com>
2020-07-22 15:18:23 +0800

    add animation top to bottom and vice versa

--- src/ContentLoader.js
+++ src/ContentLoader.js
@@ -18,21 +18,22 @@ const styles = StyleSheet.create({
 });
 
 const BackgroundComponent = (props) => {
-    return <View style={{...styles.container, backgroundColor: props.backColor}}/>;
+    const { backColor,onLayout } = props; 
+    return <View style={{ ...styles.container, backgroundColor: backColor }} onLayout={onLayout} />;
 }
 
-const dirOrigin = {
+let dirOrigin = {
     ltr: {
         left: (I18nManager.isRTL ? -1 : 1) * -width,
     },
     rtl: {
         left: (I18nManager.isRTL ? -1 : 1) * width
-    }
+    },
 }
 
-const dirDestination = {
+let dirDestination = {
     ltr: 2 * width,
-    rtl: 2 * -width
+    rtl: 2 * -width,
 }
 
 const ContentLoader = (props) => {
@@ -46,7 +47,19 @@ const ContentLoader = (props) => {
     const [animatedStyle, setAnimatedStyle] = useState({});
     
     const getAnimationElement = (forColor) => {
-        return (
+        let { dir } = props;
+        return !!['top','bottom'].find(ky=> ky === dir) ? 
+        (
+            React.createElement(Svg, { height: "100%", width: "100%" },
+            React.createElement(Defs, null,
+            React.createElement(LinearGradient, { id: "grad", x1: "0.5", y1: "0", x2: "0.5", y2: "1" },
+            React.createElement(Stop, { offset: "0", stopColor: forColor, stopOpacity: "0" }),
+            React.createElement(Stop, { offset: "0.3", stopColor: forColor, stopOpacity: "1" }),
+            React.createElement(Stop, { offset: "0.7", stopColor: forColor, stopOpacity: "1" }),
+            React.createElement(Stop, { offset: "1", stopColor: forColor, stopOpacity: "0" }))),
+            React.createElement(Rect, { x: "0", y: "0", width: "100%", height: "100%", fill: "url(#grad)" }))
+        )
+        : (
             React.createElement(Svg, { height: "100%", preserveAspectRatio: "xMinYMin slice", width: "100%", viewBox: "0 0 100 100" },
             React.createElement(Defs, null,
             React.createElement(LinearGradient, { id: "grad", x1: "0", y1: "0.5", x2: "1", y2: "0.5" },
@@ -58,10 +71,13 @@ const ContentLoader = (props) => {
         );
     }
 
-    useEffect(() => {
+    const show = ()=>{
 
+        let { dir } = props;
+        const transform = !!['top','bottom'].find(ky=> ky === dir) ? [{translateY: translateYValue }] :  [{ translateX: translateYValue }];
+        
         setAnimatedStyle({
-            transform: [{ translateX: translateYValue }],
+            transform,
             width: width,
             backgroundColor: 'transparent',
             zIndex: 2,
@@ -78,12 +94,59 @@ const ContentLoader = (props) => {
             delay,
             useNativeDriver: true,
         })).start();
-        
+
+    }
+
+    useEffect(() => {
+        let { dir } = props;
+        //for vertical wait for the onLayout to get the height
+        if(!!['top','bottom'].find(ky=> ky === dir)) return;
+        show();
     },[]);
+
 
     return (
         React.createElement(MaskedView, { maskElement: MaskedElement },
-        React.createElement(BackgroundComponent, { backColor: backColor }),
+        React.createElement(BackgroundComponent, { backColor: backColor, onLayout:ev=>{
+            let { dir } = props;
+
+            //for Horizantal will happen in useeffect
+            if(!['top','bottom'].find(ky=> ky === dir)) return;
+
+            const {layout} = ev.nativeEvent;
+            const {height} = layout;
+            if(dir === 'bottom') {
+
+                dirOrigin = {
+                    ...dirOrigin,
+                    bottom : {
+                        bottom: -height
+                    }
+                }
+    
+                dirDestination = {
+                    ...dirDestination,
+                    bottom: -2 * height
+                }
+
+            } else {
+
+                dirOrigin = {
+                    ...dirOrigin,
+                    top : {
+                        top: -height
+                    }
+                }
+    
+                dirDestination = {
+                    ...dirDestination,
+                    top: 2 * height
+                }
+            }
+
+            show();
+            
+        }}),
         React.createElement(Animated.View, { style: animatedStyle }, getAnimationElement(forColor)))
     );
 }
